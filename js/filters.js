ec47267d58303835af7a98df8f641e940fcb6cfd
Luke Carbis <luke@carb.is>
2021-11-23 06:41:26 +1000

    Force Fullscreen Mode to be on for admin pages

--- js/filters.js
+++ js/filters.js
@@ -1,5 +1,9 @@
 wp.domReady( () => {
 	const { removeEditorPanel } = wp.data.dispatch( 'core/edit-post' );
-
 	removeEditorPanel( 'post-status' );
+
+	const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' );
+	if ( isFullscreenMode ) {
+		wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' );
+	};
 } );
